a7b7492e6662362746fbbd0465ce5896ff945f68
tgiv014 <tgiv014@gmail.com>
2013-12-11 14:44:37 -0600

    testing

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+if(process.argv[2]!="test"){
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
 app.listen(3001);
 console.log('Listening on port 3000');
+}else{
+	console.log("I'm not falling for you, test");
+}
