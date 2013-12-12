f39c4879a6b2def0b920061c288114d735823586
tgiv014 <tgiv014@gmail.com>
2013-12-12 14:25:55 -0600

    limiter repair

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ if(process.argv[2]!="test"){
 
 	io.sockets.on('connection', function (socket) {
 		time=0;
-		socket.emit('news22', { num: count , next: time});
-		socket.on('clicked2', function (data) {
+		socket.emit('news23', { num: count , next: time});
+		socket.on('clicked3', function (data) {
 			count++;
 			time=data.t;
-			time+=1000;
-			socket.emit('news22', { num: count , next: time});
-			socket.broadcast.emit('news2', { num: count });
+			time+=500;
+			socket.emit('news23', { num: count , next: time});
+			socket.broadcast.emit('news3', { num: count });
 		});
 	});
 }else{
