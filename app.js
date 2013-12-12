24b10366a8c7bbec9b188b0f444af6f2c27413c9
tgiv014 <tgiv014@gmail.com>
2013-12-12 16:24:17 -0600

    socket.io has no documentation

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if(process.argv[2]!="test"){
 		var time=0;
 		socket.emit('news2', { num: count , next: time});
 		socket.on('clicked', function (data) {
-			var address = socket.handshake.address;
+			var address = socket.handshake.address.address;
 			if(users.indexOf(address) > -1){
 				console.log('ARRR! SPAMMER AHOY!');
 			}else{
