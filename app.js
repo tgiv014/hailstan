de1cb5e915ccd11bf0b2aa20fb141083db11cbe7
tgiv014 <tgiv014@gmail.com>
2013-12-11 18:07:25 -0600

    stansockets

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var express = require('express');
-var app = express();
+var app = require('express')()
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server);
 
 if(process.argv[2]!="test"){
-app.get('/', function(req, res){
-  res.send('All hail Stan!');
-});
+	server.listen(3001);
 
-app.listen(3001);
-console.log('Listening on port 3001');
+	app.get('/', function (req, res) {
+		res.sendfile(__dirname + '/index.html');
+	});
+
+	io.sockets.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+			console.log(data);
+		});
+	});
 }else{
 	console.log("I'm not falling for you, test");
 }
