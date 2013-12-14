b8934344234409b79d9a54f9d7c7dc316ca0c2ad
tgiv014 <tgiv014@gmail.com>
2013-12-13 23:53:27 -0600

    super-hacky

--- app.js
+++ app.js
@@ -6,48 +6,52 @@ var app = express()
 
 var count = 0;
 var users = [];
+var clients1;
+var total1;
 
-if(process.argv[2]!="test"){
-	server.listen(3001);
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/assets/index.html');
+});
 
-	app.get('/', function (req, res) {
-		res.sendfile(__dirname + '/assets/index.html');
-	});
-	app.get('/dev', function (req, res) {
-		var address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		if(address=="192.168.1.125"){
-			res.sendfile(__dirname + '/assets/index2.html');
+app.get('/stats', stat.page);
+
+app.get('/dev', function (req, res) {
+	var address = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	if(address=="192.168.1.125"){
+		res.sendfile(__dirname + '/assets/index2.html');
+	}else{
+		res.send("You probably shouldn't be here.");
+	}
+});
+
+app.use(express.static('assets'));
+
+io.sockets.on('connection', function (socket) {
+	var time=0;
+	socket.emit('news2', { num: count , next: time});
+	socket.on('clicked', function (data) {
+		var address = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
+		if(users.indexOf(address) > -1){
+			console.log('ARRR! SPAMMER AHOY! - '+address);
 		}else{
-			res.send("You probably shouldn't be here.");
+			count++;
+			stat.recordClick(address);
+			time=data.t;
+			time+=500;
+			socket.emit('news2', { num: count , next: time});
+			socket.broadcast.emit('news', { num: count });
+			users.push(address);
+			console.log('Legit click from '+address);
+			setTimeout(function(){
+				users.splice(users.indexOf(address),1);
+				console.log('Removed '+address+' from queue');
+			},400);
 		}
 	});
-	app.use(express.static('assets'));
-	io.sockets.on('connection', function (socket) {
-		var time=0;
-		socket.emit('news2', { num: count , next: time});
-		socket.on('clicked', function (data) {
-			var address = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
-			if(users.indexOf(address) > -1){
-				console.log('ARRR! SPAMMER AHOY! - '+address);
-			}else{
-				count++;
-				stat.recordClick(address);
-				time=data.t;
-				time+=500;
-				socket.emit('news2', { num: count , next: time});
-				socket.broadcast.emit('news', { num: count });
-				users.push(address);
-				console.log('Legit click from '+address);
-				setTimeout(function(){
-					users.splice(users.indexOf(address),1);
-					console.log('Removed '+address+' from queue');
-				},400);
-				if((count%10)==0){
-					stat.processHits();
-				}
-			}
-		});
-	});
+});
+setInterval(stat.processHits(),60000)
+if(process.argv[2]!="test"){
+	server.listen(3001);
 }else{
 	console.log("I'm not falling for you, test");
 }
--- stat.js
+++ stat.js
@@ -1,5 +1,8 @@
 var clients = [];
 var lookup = [];
+var topaddresses = [];
+var tophits = [];
+
 var recordClick = function(address){
 	id=lookup.indexOf(address);
 	if(id>-1){
@@ -21,14 +24,36 @@ var processHits = function(){
 	clients.sort(function(a,b){
 		return b.count-a.count;
 	});
-	
+
+	var total = 0;
+
 	clients.forEach(function(entry,index){
 		lookup[index]=entry.address;
+		if(index<=10){
+			tophits[index]=entry.count;
+			if(index!=10){
+				topaddresses[index]=entry.address;
+			}else{
+				topaddresses[index]='Everyone Else';
+			}
+		}else{
+			tophits[10]+=entry.count;
+		}
+		total+=entry.count;
 	});
 
 	console.log(clients);
 	console.log(lookup);
 }
 
+var page = function(req, res){
+	var content;
+	tophits.forEach(function(entry,index){
+		content+='Clicks: '+entry+' User: '+topaddresses[index]+'<br>';
+	});
+	res.send(content);
+}
+
 module.exports.recordClick = recordClick;
 module.exports.processHits = processHits;
+module.exports.page = page;
